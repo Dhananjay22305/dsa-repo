cdc2567c0a677cbd391e78b0079c2a70cf50409a
Dhananjay22305 <bhoyar.dhananjay.22@gmail.com>
2026-01-18 10:21:40 +0530

    Add function to find length of longest substring

---
+++ string/long_substring.py
@@ -0,0 +1,13 @@
+def longest(s):
+    seen = set()
+    l=0
+    ans=0
+    for r in range(len(s)):
+        while s[r] in seen:
+            seen.remove(s[l])
+            l+=1
+        seen.add(s[r])
+        ans=max(ans,r-l+1)
+    return ans
+
+print(longest("abcabcbb"))
